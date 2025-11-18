830c489d17e083968eecb0aea59ebebd13cb4dd4
dev2points <24020277@vnu.edu.vn>
2025-11-18 11:05:44 +0700

    solving with at most k labels

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 import os
+import psutil
 import sys
-from pysat.solvers import Glucose3
+from time import time
+# from pysat.solvers import Cadical195
+from pysat.solvers import Glucose4
+from pysat.pb import PBEnc
 
 def get_file_names(dataset_folder):
     base = os.path.basename(dataset_folder)
@@ -17,7 +21,7 @@ def get_file_names(dataset_folder):
             "ctr": os.path.join(dataset_folder, "CTR.TXT")
         }
     else:
-        raise ValueError("Tên folder phải bắt đầu bằng 'graph' hoặc 'scen'")
+        raise ValueError("Not a valid dataset: " + dataset_folder)
 
 def read_domain(file):
     domain = []
@@ -28,7 +32,7 @@ def read_domain(file):
                 continue
             values = list(map(int, parts[2:]))
             domain.append(values)
-    return domain
+    return domain 
 
 def read_var(file, domain):
     var = {}
@@ -40,7 +44,7 @@ def read_var(file, domain):
             idx = int(parts[0])
             subset_idx = int(parts[1])
             var[idx] = domain[subset_idx]
-    return var
+    return var # domain subset for each variable
 
 def create_var_map(var):
     var_map = {}
@@ -49,17 +53,17 @@ def create_var_map(var):
         for v in vals:
             var_map[(i, v)] = counter
             counter += 1
-    return var_map
+    return var_map # dict mapping (i, v) to variable number
 
 def build_constraints(solver, var, var_map, ctr_file):
-    # One-Hot: mỗi đỉnh chọn đúng 1 giá trị
+    # Exactly One
     for i, vals in var.items():
         solver.add_clause([var_map[(i, v)] for v in vals])
         for j in range(len(vals)):
             for k in range(j+1, len(vals)):
                 solver.add_clause([-var_map[(i, vals[j])], -var_map[(i, vals[k])]])
 
-    # Ràng buộc khoảng cách
+    # Distance constraints
     with open(ctr_file) as f:
         for line in f:
             parts = line.strip().split()
@@ -78,8 +82,31 @@ def build_constraints(solver, var, var_map, ctr_file):
                 target = int(parts[-1])
                 for vi in vals_i:
                     for vj in vals_j:
-                        if abs(vi - vj) != target:
-                            solver.add_clause([-var_map[(i, vi)], -var_map[(j, vj)]])
+                        if abs(vi - vj) == target:
+                            solver.add_clause([-var_map[(i, vi)], var_map[(j, vj)]])
+                            
+def create_label_var_map(labels, start_index):
+    label_var_map = {}
+    current = start_index
+    for lb in labels:
+        label_var_map[lb] = current
+        current += 1
+    return label_var_map
+    
+def build_label_constraints(solver, var_map, label_var_map):
+    for (i, v), varnum in var_map.items():
+        lb_varnum = label_var_map[v]
+        solver.add_clause([-varnum, lb_varnum])
+
+def add_limit_label_constraints(solver, label_var_map, max_labels):
+    label_vars = list(label_var_map.values())
+    
+    atmost_k = PBEnc.leq(lits=label_vars, weights=[1]*len(label_vars),
+                         bound=max_labels, encoding = 1) 
+
+    for clause in atmost_k.clauses:
+        solver.add_clause(clause)
+
 
 def solve_and_print(solver, var_map):
     if solver.solve():
@@ -118,15 +145,12 @@ def verify_solution_simple(assignment, ctr_file):
                     return False
     return True
 
-# -------------------------------
-# Main
-# -------------------------------
 def main():
+    start_time = time()
     if len(sys.argv) < 2:
-        print("Sử dụng: python main.py <dataset_folder>")
+        print("Use: python main.py <dataset_folder>")
         return
 
-    # tự động thêm dataset/ vào đầu
     dataset_folder = os.path.join("dataset", sys.argv[1])
 
     try:
@@ -139,16 +163,54 @@ def main():
     var = read_var(files["var"], domain)
     var_map = create_var_map(var)
 
-    solver = Glucose3()
+    solver = Glucose4()
+    # solver = Cadical195()
     build_constraints(solver, var, var_map, files["ctr"])
-    assignment = solve_and_print(solver, var_map)
 
-    # Kiểm tra kết quả
+    assignment = solve_and_print(solver, var_map)
+    if assignment is None:
+        return
     if verify_solution_simple(assignment, files["ctr"]):
-        print("\nCorrect solution!")
-        print("Number of lables used: ", len(set(assignment.values())))
+        print("Correct solution!")
+        num_lables = len(set(assignment.values()))
+        print("Number of lables used: ", num_lables)
     else:
-        print("\nIncorrect solution!")
+        print("Incorrect solution!")
+        return
+    end_time = time()
+    print(f"Time taken: {end_time - start_time:.2f} seconds")
+    process = psutil.Process(os.getpid())
+    print(f"Memory used: {process.memory_info().rss / 1024**2:.2f} MB")
+    lable_var_map = create_label_var_map(domain[0], len(var_map) + 1)
+    build_label_constraints(solver, var_map, lable_var_map)
+
+    while True:
+        start_time = time()
+        solver.delete()
+        solver = Glucose4()
+        build_constraints(solver, var, var_map, files["ctr"])
+        build_label_constraints(solver, var_map, lable_var_map)
+        add_limit_label_constraints(solver, lable_var_map,num_lables - 1)
+        print(f"\nSolving with at most {num_lables - 1} labels...")
+        build_time = time()
+        print(f"Build solver complete with {build_time - start_time:.2f} seconds")
+        assignment = solve_and_print(solver, var_map)
+        if assignment is None:
+            break
+        if verify_solution_simple(assignment, files["ctr"]):
+            print("Correct solution!")
+            new_num_lables = len(set(assignment.values()))
+            print("Number of lables used: ", new_num_lables)
+            num_lables = new_num_lables 
+            
+        else:
+            print("Incorrect solution!")
+            break
+
+        end_time = time()
+        print(f"Time taken: {end_time - start_time:.2f} seconds")
+        process = psutil.Process(os.getpid())
+        print(f"Memory used: {process.memory_info().rss / 1024**2:.2f} MB")
 
     solver.delete()
 
